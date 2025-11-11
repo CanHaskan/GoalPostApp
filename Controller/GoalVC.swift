f17ed0cac589cbabd293557fabda82b45aec8a6d
CanHaskan <canhaskan1903@hotmail.com>
2025-11-11 17:58:59 +0300

    BuildingVCs

---
+++ Controller/GoalVC.swift
@@ -0,0 +1,25 @@
+//
+//  GoalsVC.swift
+//  GoalPostApp
+//
+//  Created by Can Haskan on 11.11.2025.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+        
+    }
+
+
+}
+
