25f53946ff571f298b10b09fb3eaaa8f1db0e802
CanHaskan <canhaskan1903@hotmail.com>
2025-11-13 00:14:40 +0300

    Saving Data to Core

--- Controller/CreateGoalVC.swift
+++ Controller/CreateGoalVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CreateGoalVC: UIViewController {
+class CreateGoalVC: UIViewController, UITextViewDelegate {
     
     
     @IBOutlet weak var goalTextView: UITextView!
@@ -22,9 +22,15 @@ class CreateGoalVC: UIViewController {
         nextBtn.bindToKeyboard()
         shorTermBtn.setSelectedColor()
         longTermBtn.setDeselectedColor()
+        goalTextView.delegate = self
     }
 
     @IBAction func nextBtnWasPressed(_ sender: Any) {
+        if goalTextView.text != "" && goalTextView.text != "What is your goal?" {
+            guard let finishGoalVC = storyboard?.instantiateViewController(withIdentifier: "FinishGoalVC") as? FinishGoalVC else {return}
+            finishGoalVC.initData(description: goalTextView.text!, type: goalType)
+            presentingViewController?.presentSecondaryDetail(finishGoalVC)
+        }
     }
     
     @IBAction func shortTermBtnWasPressed(_ sender: Any) {
@@ -41,6 +47,11 @@ class CreateGoalVC: UIViewController {
     
     @IBAction func backBtnWasPressed(_ sender: Any) {
         dismissDetail()
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        goalTextView.text = ""
+        goalTextView.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
     }
     
     
