8ea36dcb7e5927b43c34f9e4e0c5b4e579c95e8c
CanHaskan <canhaskan1903@hotmail.com>
2025-11-17 01:04:57 +0300

    Undo Button Feature

--- Controller/GoalsVC.swift
+++ Controller/GoalsVC.swift
@@ -15,6 +15,13 @@ class GoalsVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var goals: [Goal] = []
+    var lastDeletedGoal: Goal?
+    var lastDeletedGoalIndex: IndexPath?
+    var deleteWorkItem: DispatchWorkItem?
+    
+    private var undoView: UIView?
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -73,9 +80,15 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
         let deleteAction = UITableViewRowAction(style: .destructive, title: "DELETE") { rowAction, indexPath in
-            self.removeGoal(atIndexPath: indexPath)
-            self.fetchCoreDataObjects()
+            self.lastDeletedGoal = self.goals[indexPath.row]
+            self.lastDeletedGoalIndex = indexPath
+            
+            self.goals.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+            self.showUndoSnackbar()
+            self.scheduleDeleteOfLastDeletedGoal(after: 3.5)
+
         }
         
         let addAction = UITableViewRowAction(style: .normal, title: "ADD") { rowAction, indexPath in
@@ -86,6 +99,139 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
         addAction.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
         return [deleteAction, addAction]
     }
+    
+
+    func scheduleDeleteOfLastDeletedGoal(after seconds: TimeInterval) {
+        deleteWorkItem?.cancel()
+
+        let workItem = DispatchWorkItem { [weak self] in
+            guard let self = self else { return }
+            self.permanentlyDeleteLastDeletedGoal()
+        }
+        deleteWorkItem = workItem
+
+        DispatchQueue.global().asyncAfter(deadline: .now() + seconds, execute: workItem)
+    }
+
+    @objc func undoDeleteTapped() {
+        deleteWorkItem?.cancel()
+        deleteWorkItem = nil
+
+        guard let restoredGoal = lastDeletedGoal, let restoredIndexPath = lastDeletedGoalIndex else {
+            hideUndoSnackbar()
+            return
+        }
+
+        goals.insert(restoredGoal, at: restoredIndexPath.row)
+
+        DispatchQueue.main.async {
+            self.tableView.insertRows(at: [restoredIndexPath], with: .automatic)
+            self.hideUndoSnackbar()
+        }
+
+        lastDeletedGoal = nil
+        lastDeletedGoalIndex = nil
+    }
+
+    private func permanentlyDeleteLastDeletedGoal() {
+        guard let goalToDelete = lastDeletedGoal else {
+            DispatchQueue.main.async {
+                self.hideUndoSnackbar()
+            }
+            return
+        }
+
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else {
+            DispatchQueue.main.async {
+                self.hideUndoSnackbar()
+            }
+            return
+        }
+
+        managedContext.delete(goalToDelete)
+
+        do {
+            try managedContext.save()
+        } catch {
+            debugPrint("Could not permanently delete: \(error.localizedDescription)")
+        }
+
+        lastDeletedGoal = nil
+        lastDeletedGoalIndex = nil
+        deleteWorkItem = nil
+
+        DispatchQueue.main.async {
+            self.hideUndoSnackbar()
+        }
+    }
+    
+    func showUndoSnackbar() {
+        hideUndoSnackbar()
+
+        guard let parent = self.view else { return }
+
+        let height: CGFloat = 56
+        let horizontalPadding: CGFloat = 12
+        let bottomPadding: CGFloat = 20 + parent.safeAreaInsets.bottom
+
+        let snackbar = UIView(frame: CGRect(x: horizontalPadding,
+                                            y: parent.bounds.height + height,
+                                            width: parent.bounds.width - horizontalPadding * 2,
+                                            height: height))
+        snackbar.backgroundColor = UIColor(red: 1.0, green: 0.35, blue: 0.35, alpha: 1.0)
+        snackbar.layer.cornerRadius = 12
+        snackbar.clipsToBounds = true
+
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        let goalName = lastDeletedGoal?.goalDescription ?? "Goal"
+        label.text = "\(goalName) deleted."
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.numberOfLines = 1
+        label.lineBreakMode = .byTruncatingTail
+        snackbar.addSubview(label)
+
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("UNDO", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(undoDeleteTapped), for: .touchUpInside)
+        snackbar.addSubview(button)
+
+        parent.addSubview(snackbar)
+        self.undoView = snackbar
+
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: snackbar.leadingAnchor, constant: 16),
+            label.centerYAnchor.constraint(equalTo: snackbar.centerYAnchor),
+            label.trailingAnchor.constraint(lessThanOrEqualTo: button.leadingAnchor, constant: -8),
+
+            button.trailingAnchor.constraint(equalTo: snackbar.trailingAnchor, constant: -12),
+            button.centerYAnchor.constraint(equalTo: snackbar.centerYAnchor)
+        ])
+
+        DispatchQueue.main.async {
+            UIView.animate(withDuration: 0.25) {
+                snackbar.frame.origin.y = parent.bounds.height - height - bottomPadding
+            }
+        }
+    }
+
+    func hideUndoSnackbar() {
+        guard let snackbar = self.undoView, let parent = self.view else { return }
+        DispatchQueue.main.async {
+            UIView.animate(withDuration: 0.18, animations: {
+                snackbar.frame.origin.y = parent.bounds.height + snackbar.frame.height
+            }, completion: { _ in
+                snackbar.removeFromSuperview()
+            })
+        }
+        self.undoView = nil
+    }
+
+
 }
 
 extension GoalsVC {
@@ -103,7 +249,6 @@ extension GoalsVC {
         
         do {
             try managedContext.save()
-            print("Successfully set progress")
         } catch {
             debugPrint("Could not set progress: \(error.localizedDescription)")
         }
@@ -116,7 +261,6 @@ extension GoalsVC {
         
         do{
             try managedContext.save()
-            print ("Succesfull")
         } catch {
             debugPrint("Could not remove: \(error.localizedDescription)")
         }
@@ -130,7 +274,6 @@ extension GoalsVC {
         
         do {
             goals = try managedContext.fetch(fetchRequest)
-            print("Succesfully fetched data.")
             completion(true)
         } catch {
             debugPrint("Could not fetch: \(error.localizedDescription)")
